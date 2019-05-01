5cb0baf2296127b8854acaff02c49bb7cb3c74d7
Patrick Glen Kennedy <pkennedytx1@Patricks-MacBook-Pro.local>
2019-05-01 16:40:45 -0500

    added small div with links to site and github

---
+++ src/components/Links/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Links () {
+  return <div className="links"><a href="pkennedytx1.com">Creator Website</a> | <a href="https://github.com/pkennedytx1/Clicky-game">Github Link</a></div>
+}
+
+export default Links;
